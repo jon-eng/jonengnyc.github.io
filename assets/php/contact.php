91f3b13c894e9b383563333327ff2b0a37f2469a
Jonathan Eng <jon.kh.eng@gmail.com>
2019-07-10 15:40:19 -0400

    Initial commit

---
+++ assets/php/contact.php
@@ -0,0 +1,36 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+if(isset($_POST['message'])){
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+    
+	
+	$to      = 'jon.kh.eng@gmail.com';
+	$subject = 'Site Contact Form';
+
+	$headers = 'From: '. $email . "\r\n" .
+    'Reply-To: '. $email . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+	$status = mail($to, $subject, $message, $headers);
+
+	if($status == TRUE){	
+		$res['sendstatus'] = 'done';
+	
+		//Edit your message here
+		$res['message'] = 'Form Submission Successful';
+    }
+	else{
+		$res['message'] = 'Failed to send mail. You can email me at jon.kh.eng@gmail.com';
+	}
+	
+	
+	echo json_encode($res);
+}
+
+?>
